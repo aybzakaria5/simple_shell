a90fe8d1eeede401d00567db61f20d9ecc7d2ed7
faty0 <fatyboubakri31@gmail.com>
2023-05-23 13:36:31 +0000

    process_exec file

--- shell.h
+++ shell.h
@@ -1,12 +1,24 @@
+#ifndef SHELL_H
+#define SHELL_H
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <signal.h>
 extern char **environ;
 char *_getenv(const char *env_name);
 char **parse(char *buffer, char *delim);
 void execute_command(char **ar_parsed);
-void readPrompt();
+void readPrompt(void);
 char *getpath(char *cmd);
+void _puts(char *str);
+int _putchar(char c);
+int _strcmp(const char *s1, const char *s2);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strdup(char *str);
+int _strlen(char *s);
+#endif /* #define SHELL_H */
