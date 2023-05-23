66b0348cc4280883f0c6e6414380af726bed16e2
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-23 17:03:29 +0200

    fixing betty

--- process_exec.c
+++ process_exec.c
@@ -7,8 +7,8 @@
 
 void handl_ctrlc(int signal)
 {
-    (void)signal;
-    puts("($) ");
+	(void)signal;
+	puts("($) ");
 }
 
 /**
@@ -17,85 +17,79 @@ void handl_ctrlc(int signal)
  */
 void readPrompt(void)
 {
-    size_t buf_size = 0;
-    char **ar_parsed;
-    int n_reads;
-    char *buf = NULL;
-  signal(SIGINT, handl_ctrlc);
+	size_t buf_size = 0;
+	char **ar_parsed;
+	int n_reads;
+	char *buf = NULL;
 
-    while (1)
-    {
-        /* type prompt */
-        puts("($) ");
-        /* reads input */
-        n_reads = getline(&buf, &buf_size, stdin);
-    
+	signal(SIGINT, handl_ctrlc);
 
-        /* handle empty input */
+	while (1)
+	{
+		/* type prompt */
+		puts("($) ");
+		/* reads input */
+		n_reads = getline(&buf, &buf_size, stdin);
 
-        if (strcmp(buf, "\n") == 0)
-        {
-            continue;
-        }
-        if (n_reads == -1)
-        {
-            putchar('\n');
-            free(buf);
-            exit(1);
-        }
-        /* parse command */
-        ar_parsed = parse(buf, " \t\n");
+		/* handle empty input */
 
-        /* handle exit command */
-        if (strcmp(ar_parsed[0], "exit") == 0)
-        {
-            free(buf);
-            free(ar_parsed);
-            exit(0);
-        }
-        /*handl the builtin env*/
+		if (strcmp(buf, "\n") == 0)
+		{
+			continue;
+		}
 
-        /* start command invoking */
-        execute_command(ar_parsed);
-        /* free args before next parsing */
-        free(ar_parsed);
-    }
+		if (n_reads == -1)
+		{
+			putchar('\n');
+			free(buf);
+			exit(1);
+		}
+
+		ar_parsed = parse(buf, " \t\n");
+
+		if (strcmp(ar_parsed[0], "exit") == 0)
+		{
+			free(buf);
+			free(ar_parsed);
+			exit(0);
+		}
+		execute_command(ar_parsed);
+		free(ar_parsed);
+	}
 }
 /**
  * execute_command - a function that executes the command line
  * @ar_parsed: the array of commands we want to excute
  *
  */
-/**
- * builtin_env - a fucntion that prints the envirment vars in the current path
- */
 
 void execute_command(char **ar_parsed)
 {
-    char *cmd;
-    int status;
-    pid_t pid;
-    cmd = getpath(ar_parsed[0]);
+	char *cmd;
+	int status;
+	pid_t pid;
 
-    if (cmd)
-    {
-        pid = fork();
-        if (pid == 0)
-        {
-            /* execute command */
-            execve(cmd, ar_parsed, environ);
-            exit(0);
-        }
+	cmd = getpath(ar_parsed[0]);
 
-        else
-        {
-            wait(&status);
-        }
-    }
-    else
-    {
-        puts("command not found");
-    }
-    if (cmd != ar_parsed[0])
-        free(cmd);
+	if (cmd)
+	{
+		pid = fork();
+		if (pid == 0)
+		{
+			/* execute command */
+			execve(cmd, ar_parsed, environ);
+			exit(0);
+		}
+
+		else
+		{
+			wait(&status);
+		}
+	}
+	else
+	{
+		puts("command not found");
+	}
+	if (cmd != ar_parsed[0])
+		free(cmd);
 }
