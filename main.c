c46d1ad0d2ebdf227f928514330ebd147ed5a039
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2023-05-24 15:23:57 +0200

    interactive plus fixing commands exits

--- main.c
+++ main.c
@@ -1,15 +1,41 @@
 #include "shell.h"
-/**
- *  main- entery point
- *@ac: args counter
- *@av: args values
- *Return: returns 0 if sucess
- */
 
+int is_interactive;
+
+/**
+ * main - entry point
+ * @ac: args counter
+ * @av: args values
+ * Return: returns 0 if success
+ */
 int main(int ac, char **av)
 {
-	(void) ac;
-	(void)av;
-	readPrompt();
-	return (0);
+		char *line = NULL;
+		char **args;
+        size_t bufsize = 0;
+        ssize_t nreads;
+		(void)ac;
+    	(void)av;
+    is_interactive = isatty(STDIN_FILENO);
+    if (is_interactive)
+    {
+        readPrompt();
+    }
+    else
+    {
+
+        while ((nreads = getline(&line, &bufsize, stdin)) != -1)
+        {
+            if (nreads > 0 && line[nreads - 1] == '\n')
+                line[nreads - 1] = '\0';
+
+            args = parse(line, " \t\n");
+            execute_command(args);
+            free(args);
+        }
+
+        free(line);
+    }
+
+    return (0);
 }
